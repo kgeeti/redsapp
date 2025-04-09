21f32d96f6e4622ca8bf8756c35dfce308f4e75c
KGe <kge@terra.com.br>
2025-04-09 14:15:51 -0300

    Inicialização do Projeto

---
+++ src/main/java/br/eti/kge/redsapp/RedsappApplication.java
@@ -0,0 +1,13 @@
+package br.eti.kge.redsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedsappApplication.class, args);
+	}
+
+}
